6c53b3f34aae3a5a317c9b92ec64021224ff165f
Nayan Bagale <nvbagale@gmail.com>
2023-09-26 00:25:04 +0530

    git push

--- src/Components/Questions/previous-questions/Previous.jsx
+++ src/Components/Questions/previous-questions/Previous.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import { UserAuth } from "../../../ContextApi/AuthContext";
 import { db } from "../../../firebase/firebase";
 import { collection, getDocs } from "firebase/firestore";
+import { RWebShare } from "react-web-share";
+import { useLocation } from "react-router-dom";
 
 const Previous = () => {
   const { user } = UserAuth();
   const [question, setQuestions] = useState([]);
 
   const userCollectionRef = collection(db, "questions");
+
+  const location = window.location.origin;
 
   useEffect(() => {
     const getData = async () => {
@@ -27,11 +31,29 @@ const Previous = () => {
       <div>
         {question &&
           question.map((items) => {
-            return (items.data.map((item) => {
-              return <li key={item.data.key}>{item.data.question}</li>;
-            }));
+            return items.data.map((item) => {
+              return (
+                <div key={item.data.key}>
+                  {item.data.question}
+                  <RWebShare
+                    data={{
+                      text: "Questions",
+                      url: `${location}/form/${items.qid}`,
+                      title: "Flamingos",
+                    }}
+                    onClick={() => console.log("shared successfully!")}
+                  >
+                    <button>Share 🔗</button>
+                  </RWebShare>
+                </div>
+              );
+            });
           })}
       </div>
+      {/* <div className=" flex flex-col text-2xl text-white">
+        <div>Question Share 🔗</div>
+        <div>Question Share 🔗</div>
+      </div> */}
     </div>
   );
 };
