3147491739faf91aca4a02a0f8dcebbc25445d49
Nayan Bagale <nvbagale@gmail.com>
2023-10-12 02:41:46 +0530

    git push

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,13 +3,15 @@ import { UserAuth } from "../ContextApi/AuthContext";
 
 import TopScore from "../Components/Dashboard/Top/TopScore";
 import { Toaster } from "react-hot-toast";
+import DashCard from "../Components/Dashboard/DashCard";
 
 const Dashboard = () => {
   const { quesAttemted } = UserAuth();
 
   return (
     <main className=" flex flex-col gap-4 my-4 w-full min-h-[80vh] text-white text-3xl">
-      <TopScore quesAttemted={quesAttemted} />
+      {/* <TopScore quesAttemted={quesAttemted} /> */}
+      <DashCard quesAttemted={quesAttemted} />
       <div className=" text-base md:text-xl">
         <Toaster />
       </div>
