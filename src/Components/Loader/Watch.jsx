c3bbd84e63317d5e221b20ee9ae439ee5d383ed1
Nayan Bagale <nvbagale@gmail.com>
2023-12-09 01:19:46 +0530

    Style: Changed Loading Animation from Watch to Progress Bar with Logo Beating Animation.

--- src/Components/Loader/Watch.jsx
+++ src/Components/Loader/Watch.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Watch } from "react-loader-spinner";
+import { Watch, ProgressBar } from "react-loader-spinner";
+import { motion } from "framer-motion";
 
 const LoaderWatch = () => {
   return (
-    <div className="flex items-center justify-center h-[90vh]">
-      <Watch
+    <div className="flex flex-col items-center justify-center h-[90vh]">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 0.4, scale: 1 }}
+        transition={{ duration: 0.5, repeat: Infinity, repeatType: "reverse" }}
+      >
+        <img src="/logo1.png" alt="logo" className=" w-[8rem]" />
+      </motion.div>
+      <ProgressBar
+        height="80"
+        width="80"
+        ariaLabel="progress-bar-loading"
+        wrapperStyle={{}}
+        wrapperClass="progress-bar-wrapper"
+        borderColor="white"
+        barColor="#51E5FF"
+      />
+      {/* <Watch
         height="80"
         width="80"
         radius="48"
@@ -13,7 +30,7 @@ const LoaderWatch = () => {
         wrapperStyle={{}}
         wrapperClassName=""
         visible={true}
-      />
+      /> */}
     </div>
   );
 };
